3b9cb78b24dc76c05de1315c6959e5493995dd25
jPascal <devnull@localhost>
2012-02-06 14:28:49 -0500

    Installer Version 3.3.0.0

--- indra/llcommon/llversionserver.h
+++ indra/llcommon/llversionserver.h
@@ -28,9 +28,9 @@
 #define LL_LLVERSIONSERVER_H
 
 const S32 LL_VERSION_MAJOR = 3;
-const S32 LL_VERSION_MINOR = 2;
-const S32 LL_VERSION_PATCH = 9;
-const S32 LL_VERSION_BUILD = 1;
+const S32 LL_VERSION_MINOR = 3;
+const S32 LL_VERSION_PATCH = 0;
+const S32 LL_VERSION_BUILD = 0;
 
 const char * const LL_CHANNEL = "Zen";
 
