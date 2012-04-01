b8f54bb7bff276543fbe7eceb0c3a4298a1557ba
jPascal <devnull@localhost>
2012-04-01 17:34:51 -0400

    MAINT-778, MAINT-793 Fix for crash when enabling Debug GL on Mobile Intel 4 Express. Don't pop up driver update notification if GPU class is < 1

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1002,7 +1002,7 @@ bool LLAppViewer::init()
 	
 #if LL_WINDOWS
 	if (gGLManager.mIsIntel && 
-		LLFeatureManager::getInstance()->getRecommendedValue("WindLightUseAtmosShaders") > 0 &&
+		LLFeatureManager::getInstance()->getGPUClass() > 0 &&
 		gGLManager.mGLVersion <= 3.f)
 	{
 		LLNotificationsUtil::add("IntelOldDriver");
